ec4b93bf0c1f05adaead5a79f078a5543e0209b1
Kunal Marwaha <marwahaha@berkeley.edu>
2014-10-19 12:22:41 -0700

    working on integrations with chat

--- pj/bearforceone/js/supporters.js
+++ pj/bearforceone/js/supporters.js
@@ -1,12 +1,3 @@
-console.log('hi');
-
-
-
-//names should be first and last initial
-//majors should vary more (be sure to change BS to BA if appropriate) - think political science, economics, other normal majors
-//in general, have more people - have enough from each school, at least one from each school/class tbh
-//ratings should vary more (not all 3.2), make them X.Y from 2.0 to 5.0). leave numbers as string
-//cheers should vary more, keep them high, nice numbers - if they're over 1000 then comma separate values. leave as string
 
 permhosts = new Array();
 permhosts.push({
@@ -15,13 +6,13 @@ permhosts.push({
     year: "First",
     phoneNumber: "+13103879406",
     emailAddress: "andreaskari@gmail.com",
-    name: "Andre",
+    name: "Andre A",
     password: "991A664DA8",
     callingHours: ["0100", "2300"],
-    major: 'CS',
+    major: 'EECS',
     cheers: '104',
-    rating: '3.2',
-    degree: 'BA',
+    rating: '4.9',
+    degree: 'BS',
 
 });
 permhosts.push({
@@ -30,12 +21,12 @@ permhosts.push({
     year: "Third",
     phoneNumber: "+19164024661",
     emailAddress: "kmar@berkeley.edu",
-    name: "Kunal",
+    name: "Kunal M",
     password: "991A664DA8",
     callingHours: ["0100", "2300"],
     major: 'Physics',
-    cheers: '104',
-    rating: '3.2',
+    cheers: '738',
+    rating: '5.0',
     degree: 'BS',
 
 });
@@ -45,18 +36,15 @@ permhosts.push({
     year: "Third",
     phoneNumber: "+13103879406",
     emailAddress: "zz@berkeley.edu",
-    name: "Zach",
+    name: "Zach Z",
     password: "991A664DA8",
     callingHours: ["0100", "2300"],
     major: 'EECS',
-    cheers: '104',
-    rating: '3.2',
+    cheers: '1,133',
+    rating: '5.0',
     degree: 'BS',
 
 });
-
-
-
 
 hosts = new Array();
 
@@ -66,12 +54,12 @@ hosts.push({
     year: "Third",
     phoneNumber: "+13103879406",
     emailAddress: "hannahsim@gmail.com",
-    name: "Hannah",
+    name: "Hannah S",
     password: "4991DDAD0C",
     callingHours: ["0800", "1000"],
-    major: 'CS',
-    cheers: '104',
-    rating: '3.2',
+    major: 'Psychology',
+    cheers: '1104',
+    rating: '4.5',
     degree: 'BS',
 
 });
@@ -81,30 +69,28 @@ hosts.push({
     year: "Graduate",
     phoneNumber: "+13103879406",
     emailAddress: "brandonb@gmail.com",
-    name: "Brandon",
+    name: "Brandon B",
     password: "64D7F01E5D",
     callingHours: ["1700", "1900"],
-    major: 'CS',
-    cheers: '104',
-    rating: '3.2',
-    degree: 'MS Candidate',
+    major: '',
+    cheers: '1781',
+    rating: '3.8',
+    degree: 'M.Eng. Candidate',
 
 });
-
 hosts.push({
     school: "Santa Clara",
     problemFocus: "",
     year: "Second",
     phoneNumber: "+13103879406",
     emailAddress: "melolovesu@gmail.com",
-    name: "Melody",
+    name: "Melody A",
     password: "59C05B5FA3",
     callingHours: ["1300", "1400"],
-    major: 'CS',
-    cheers: '104',
-    rating: '3.2',
+    major: 'English',
+    cheers: '571',
+    rating: '4.4',
     degree: 'BS',
-
 });
 hosts.push({
     school: "UC Berkeley",
@@ -112,14 +98,13 @@ hosts.push({
     year: "First",
     phoneNumber: "+13103879406",
     emailAddress: "guptadesai@gmail.com",
-    name: "Gupta",
+    name: "Gupta D",
     password: "75C8236AC2",
     callingHours: ["0600", "0800"],
-    major: 'CS',
-    cheers: '104',
+    major: 'EE',
+    cheers: '812',
     rating: '3.2',
     degree: 'BS',
-
 });
 hosts.push({
     school: "Stanford",
@@ -127,12 +112,12 @@ hosts.push({
     year: "First",
     phoneNumber: "+13103879406",
     emailAddress: "lailajenson@gmail.com",
-    name: "Laila",
+    name: "Laila J",
     password: "A9974D0B7A",
     callingHours: ["1100", "1300"],
-    major: 'CS',
-    cheers: '104',
-    rating: '3.2',
+    major: 'History',
+    cheers: '341',
+    rating: '3.9',
     degree: 'BS',
 
 });
@@ -142,12 +127,12 @@ hosts.push({
     year: "Fourth",
     phoneNumber: "+13103879406",
     emailAddress: "jtisdale@gmail.com",
-    name: "Jasmine",
+    name: "Jasmine T",
     password: "408DFA2769",
     callingHours: ["1300", "1500"],
-    major: 'CS',
+    major: 'Business',
     cheers: '104',
-    rating: '3.2',
+    rating: '4.2',
     degree: 'BS',
 
 });
@@ -157,11 +142,11 @@ hosts.push({
     year: "Fifth",
     phoneNumber: "+13103879406",
     emailAddress: "n.bryanson@gmail.com",
-    name: "Nathan",
+    name: "Nathan B",
     password: "D2A9750CF1",
     callingHours: ["0300", "0500"],
-    major: 'CS',
-    cheers: '104',
+    major: 'Biology',
+    cheers: '2,315',
     rating: '3.2',
     degree: 'BS',
 
@@ -175,9 +160,9 @@ hosts.push({
     name: "Ariana",
     password: "6B5EF579FB",
     callingHours: ["2100", "2300"],
-    major: 'CS',
-    cheers: '104',
-    rating: '3.2',
+    major: 'Astronomy',
+    cheers: '954',
+    rating: '3.9',
     degree: 'BS',
 
 });
@@ -190,9 +175,9 @@ hosts.push({
     name: "Kayla",
     password: "C80954873B",
     callingHours: ["2000", "2200"],
-    major: 'CS',
-    cheers: '104',
-    rating: '3.2',
+    major: 'EE',
+    cheers: '2194',
+    rating: '4.2',
     degree: 'Ph.D Candidate',
 
 });
@@ -206,7 +191,7 @@ hosts.push({
     password: "1234567890",
     callingHours: ["0800", "1400"],
     major: "EECS",
-    cheers: '8,541',
+    cheers: '1,541',
     rating: '5.0',
     degree: 'BS',
 
@@ -240,10 +225,162 @@ hosts.push({
     rating: '4.3',
     degree: 'BA',
 });
+hosts.push({
+    school: "Stanford",
+    problemFocus: "",
+    year: "Fifth",
+    phoneNumber: "+13103879406",
+    emailAddress: "icmandy@stanford.edu",
+    name: "Mandy Cheshire",
+    password: "1234567890",
+    callingHours: ["1200", "2000"],
+    major: "Business",
+    cheers: '2322',
+    rating: '4.6',
+    degree: 'BA',
+});
+hosts.push({
+    school: "UC Berkeley",
+    problemFocus: "",
+    year: "Third",
+    phoneNumber: "+13103879406",
+    emailAddress: "ericphil@berkeley.edu",
+    name: "Eric Phillips",
+    password: "1234567890",
+    callingHours: ["0800", "1800"],
+    major: "Economics",
+    cheers: '832',
+    rating: '4.1',
+    degree: 'BA',
+});
+hosts.push({
+    school: "UC Berkeley",
+    problemFocus: "",
+    year: "Second",
+    phoneNumber: "+13103879406",
+    emailAddress: "emmalup@berkeley.edu",
+    name: "Emma Lupin",
+    password: "1234567890",
+    callingHours: ["1200", "2000"],
+    major: "Biology",
+    cheers: '1311',
+    rating: '3.9',
+    degree: 'BS',
+});
+hosts.push({
+    school: "UC Berkeley",
+    problemFocus: "",
+    year: "Fourth",
+    phoneNumber: "+13103879406",
+    emailAddress: "sander@berkeley.edu",
+    name: "Sander Griffin",
+    password: "1234567890",
+    callingHours: ["0400", "1600"],
+    major: "Chemistry",
+    cheers: '279',
+    rating: '4.2',
+    degree: 'BS',
+});
+hosts.push({
+    school: "Stanford",
+    problemFocus: "",
+    year: "Fourth",
+    phoneNumber: "+13103879406",
+    emailAddress: "jzau@stanford.edu",
+    name: "Jessica Zau",
+    password: "1234567890",
+    callingHours: ["1600", "2300"],
+    major: "Art History",
+    cheers: '1593',
+    rating: '4.6',
+    degree: 'BA',
+});
+hosts.push({
+    school: "Santa Clara",
+    problemFocus: "",
+    year: "Third",
+    phoneNumber: "+13103879406",
+    emailAddress: "kavyaw@scu.edu",
+    name: "Kavya Walia",
+    password: "1234567890",
+    callingHours: ["0100", "1300"],
+    major: "Physics",
+    cheers: '3409',
+    rating: '4.9',
+    degree: 'BS',
+});
+hosts.push({
+    school: "UC Berkeley",
+    problemFocus: "",
+    year: "Fourth",
+    phoneNumber: "+13103879406",
+    emailAddress: "meyers@berkeley.edu",
+    name: "Andy Meyers",
+    password: "1234567890",
+    callingHours: ["1000", "1600"],
+    major: "Sociology",
+    cheers: '541',
+    rating: '3.9',
+    degree: 'BA',
+});
+hosts.push({
+    school: "Santa Clara",
+    problemFocus: "",
+    year: "Graduate",
+    phoneNumber: "+13103879406",
+    emailAddress: "dkraus@scu.edu",
+    name: "David Kraus",
+    password: "1234567890",
+    callingHours: ["1000", "1600"],
+    major: "Psychology",
+    cheers: '91',
+    rating: '4.5',
+    degree: 'BA',
+});
+hosts.push({
+    school: "UC Berkeley",
+    problemFocus: "",
+    year: "Third",
+    phoneNumber: "+13103879406",
+    emailAddress: "helena@berkeley.edu",
+    name: "Helen Abrams",
+    password: "1234567890",
+    callingHours: ["0800", "1700"],
+    major: "Math",
+    cheers: '431',
+    rating: '4.4',
+    degree: 'BA',
+});
+hosts.push({
+    school: "Santa Clara",
+    problemFocus: "",
+    year: "First",
+    phoneNumber: "+13103879406",
+    emailAddress: "jenm@gmail.com",
+    name: "Jenna M",
+    password: "64D7F01E5D",
+    callingHours: ["1000", "1400"],
+    major: 'Engineering',
+    cheers: '3121',
+    rating: '4.8',
+    degree: 'BS',
 
+});
+hosts.push({
+    school: "Santa Clara",
+    problemFocus: "",
+    year: "Fifth",
+    phoneNumber: "+13103879406",
+    emailAddress: "julianm@scu.edu",
+    name: "Julian M",
+    password: "64D7F01E5D",
+    callingHours: ["0600", "1200"],
+    major: 'Business',
+    cheers: '1562',
+    rating: '4.6',
+    degree: 'BA',
 
-
-
+});
 
 
 
@@ -292,7 +429,7 @@ function addmatches() {
   console.log(info);
   
   //used to debug, see all users in system
-  info.curhosts = hosts.concat(permhosts);
+  // info.curhosts = hosts.concat(permhosts);
   
   for (var h in info.curhosts) {
     ho = info.curhosts[h];
@@ -301,10 +438,12 @@ function addmatches() {
     } else {
       callme = '';
     }
+    img = ho.img || 'http://cache.spradshirt.net/Public/Common/images/profile-pic-placeholder_130x130.png';
+
   $(".main-contain").append('\
               <div class="portfoliocard noselect supporter-' + ho.name + '">\
                 <div class="coverphoto"></div>\
-                <div class="profile_picture"></div>\
+                <div class="profile_picture profpic-' + ho.name.split(" ")[0] + '"></div>\
                 <div class="left_col">\
                     <div class="followers">\
                         <div class="cheers">' + ho.cheers.toString() + '</div>\
